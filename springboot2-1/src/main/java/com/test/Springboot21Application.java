061559516335d997f734a9695232d31fb53ef0b6
AM <AM@DESKTOP-UE5TKLO>
2022-06-08 17:50:45 +0530

    Springboot CRUD operations

---
+++ springboot2-1/src/main/java/com/test/Springboot21Application.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot21Application.class, args);
+		System.out.println("Hai");
+	}
+
+}
---
+++ springboot2-1/src/test/java/com/test/Springboot21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
